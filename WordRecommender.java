52ba788faddbd5adcde20484cf00d86f7d99e194
robinhu <hu28@seas.upenn.edu>
2025-11-10 15:24:56 -0500

    load dictionary

--- WordRecommender.java
+++ WordRecommender.java
@@ -1,8 +1,22 @@
+import java.util.*;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
 public class WordRecommender {
-
+    private ArrayList<String> dictionary;
     public WordRecommender(String dictionaryFile) {    
       // TODO: change this!
+        this.dictionary=new ArrayList<String>();
+        try (BufferedReader reader=new BufferedReader(new FileReader(dictionaryFile))){
+            String line;
+            while((line=reader.readLine())!=null){
+                this.dictionary.add(line.trim().toLowerCase());
+            }
+        }catch(IOException e){
+            System.err.println("Error Loading dictionary "+e.getMessage());
+        }
     }
+    private
   
     public double getSimilarity(String word1, String word2) {
       // TODO: change this!
@@ -11,7 +25,9 @@ public class WordRecommender {
   
     public ArrayList<String> getWordSuggestions(String word, int tolerance, double commonPercent, int topN) {
       // TODO: change this!
-      return null;
+        ArrayList<String> candidate = new ArrayList<String>();
+
+        return null;
     }
   
     // You can of course write other methods as well.
