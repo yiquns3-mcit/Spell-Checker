929583cbd0567826be7a73cf28ff30dbd39c85d4
yiquns3 <yiquns3@seas.upenn.edu>
2025-11-09 20:54:42 -0500

    YS: SpellChecker.java (1st Version)

--- SpellChecker.java
+++ SpellChecker.java
@@ -5,10 +5,20 @@ import java.util.*;
 public class SpellChecker {
     // Use this field everytime you need to read user input
     private Scanner inputReader; // DO NOT MODIFY
+    private ArrayList<String> engDictionary;
+    private ArrayList<String> wordSuggestion;
+    private ArrayList<String> inputWordBank;
+    private ArrayList<String> outputWordBank;
+    private WordRecommender recommender;
 
     public SpellChecker() {
         inputReader = new Scanner(System.in); // DO NOT MODIFY - must be included in this method
         // TODO: Complete the body of this constructor, as necessary.
+        this.engDictionary = new ArrayList<String>();
+        this.wordSuggestion = new ArrayList<String>();
+        this.inputWordBank = new ArrayList<String>();
+        this.outputWordBank = new ArrayList<String>();
+        this.recommender = null;
     }
 
     public void start() {
@@ -24,14 +34,39 @@ public class SpellChecker {
         String outputFile = text.substring(0, text.length() - 4) + "_chk.txt";
         System.out.printf("Spell checking for '%s' will be output in '%s'.%n", text, outputFile);
 
-        // (YS) Step 2:
+        // (YS) Step 2: Update inputWordBank and engDictionary (fill in with given .txt files) and WordRecommender
+        storeWordInArrayList(dict,engDictionary);
+        storeWordInArrayList(text,inputWordBank);
+        recommender = new WordRecommender(dict);
+
+        // (YS) Step 3: Pick each word from the inputWordBank and store the updated result into outputWordBank (LOOP)
+        // looping
+        for (String word: inputWordBank){
+            // Step 3.1: Check if misspelled (yes: continue; no: write down and pick next word)
+            if (engDictionary.contains(word)) {
+                // (not misspell) directly put it into output word bank
+                outputWordBank.add(word);
+            } else {
+                // (misspell) use another word follow by further instruction
+                // Step 3.2: try to get word suggestions and store them
+                getSuggestion(word);
+                // Step 3.3: check if there is any suggestion
+                boolean hasSuggestion = (wordSuggestion.size() != 0);
+                // Step 3.4: modify the misspelled word based on different cases (hasSuggestion = True/False)
+                // the following method will lead user to choose a word based on different cases
+                // and store the final result in the outputWordBank
+                handleMisspell(word, hasSuggestion);
+            }
+        }
+        // (YS) Step 4: Use the outputWordBank to form the output .txt file
+        createOutputFile(outputWordBank, outputFile);
 
         inputReader.close();  // DO NOT MODIFY - must be the last line of this method!
     }
 
     // You can of course write other methods as well.
 
-    // (YS) Method: Ask user to enter the valid .txt file name
+    // (YS) Single Method: Ask user to enter the valid .txt file name
     private String getValidFile(String prompt) {
         while (true) {
             System.out.println(prompt);
@@ -45,6 +80,123 @@ public class SpellChecker {
         }
     }
 
-    // (YS) Method: Extract words one by one from the text file
+    // (YS) Single Method: Read the file and store each word into target wordBank
+    private void storeWordInArrayList(String fileName, ArrayList<String> wordBank){
+        try {
+            FileInputStream inputFile = new FileInputStream(fileName);
+            Scanner fileReader= new Scanner(inputFile);
+            while (fileReader.hasNext()) {
+                String word = fileReader.next();
+                wordBank.add(word);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+    }
+
+    // (YS) Single Method: update the wordSuggestion ArrayList
+    private void getSuggestion(String word) {
+        // parameters setup for getWordSuggestions method
+        int tolerance = 2;
+        double commonPercent = 0.8;
+        int topN = 4;
+        wordSuggestion = recommender.getWordSuggestions(word, tolerance, commonPercent, topN);
+    }
+
+    // (YS) Main Method: Enter "r"/"a"/"t" (options) to handle the misspelled word based on different cases
+    private void handleMisspell(String misspelledWord, boolean hasSuggestion){
+        // text prompt setup
+        System.out.printf(Util.MISSPELL_NOTIFICATION, misspelledWord);
+        System.out.println();
+        if (hasSuggestion){
+            System.out.println("The following suggestions are available:");
+            // print out each suggestion
+            for (int i = 0; i < wordSuggestion.size(); i++){
+                String word = wordSuggestion.get(i);
+                System.out.printf("%d. '%s'%n", i+1, word);
+            }
+            // print out prompt
+            System.out.println("Press 'r' to replace, 'a' to accept, and 't' to enter a replacement manually.");
+        } else {
+            // print out prompt
+            System.out.println("There are no suggestions in our dictionary for this word.");
+            System.out.println("Press 'a' to accept, or press 't' to enter a replacement manually.");
+        }
+        // repeat until user gives the valid input
+        while (true) {
+            try {
+                // record user option selection
+                String choice = inputReader.next();
+                // different modification method for different option selection
+                if (choice.equals("r") && hasSuggestion) {
+                    // option "r": user choose one word suggestion
+                    System.out.println("Your word will be replaced with the suggestion you choose.");
+                    String r = wordReplace();
+                    outputWordBank.add(r);
+                    return;
+                } else if (choice.equals("a")) {
+                    // option "a": user accept the misspelled word
+                    outputWordBank.add(misspelledWord);
+                    return;
+                } else if (choice.equals("t")) {
+                    // option "t": user write down the alternative word by himself
+                    String t = wordCustom();
+                    outputWordBank.add(t);
+                    return;
+                } else {
+                    // user type the wrong option
+                    System.out.printf("Please choose one of the valid options.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Please choose one of the valid options.");
+            }
+        }
+    }
+
+    // (YS) Sub Method: user enter "r" to replace the word based on word suggestion
+    private String wordReplace(){
+        System.out.println("Enter the number corresponding to the word that you want to use for replacement.");
+        while (true) {
+            try {
+                System.out.print(">> ");
+                int num = inputReader.nextInt();
+                if (num > wordSuggestion.size() || num < 1) {
+                    System.out.println("Please enter a valid number.");
+                } else {
+                    String newWord = wordSuggestion.get(num-1);
+                    return newWord;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a valid number.");
+            }
+        }
+    }
+
+    // (YS) Sub Method: user enter "t" to enter a replacement manually
+    private String wordCustom(){
+        System.out.println("Please type the word that will be used as the replacement in the output file.");
+        while (true) {
+            try {
+                System.out.print(">> ");
+                String custom = inputReader.next();
+                return custom;
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a valid word.");
+            }
+        }
+    }
+
+    // (YS) Single Method: Use all the updated words to form a new .txt file
+    private void createOutputFile(ArrayList<String> words, String fileName){
+        String content = String.join(" ", words);
+        try (FileOutputStream outputFile = new FileOutputStream(fileName)){
+            for (String word : words) {
+                outputFile.write(word.getBytes());
+                outputFile.write(' ');
+            }
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
 
 }
